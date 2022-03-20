5678b850c30b40f2a293d8570ca605d5202e32a5
songhaibei <songhaibei7305@163.com>
2022-03-20 23:26:54 +0800

    返回数据给上一个活动

--- app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
@@ -1,12 +1,32 @@
 package com.jingwei.songhaibei.myapplication;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
+import androidx.appcompat.app.AppCompatActivity;
+
 public class FirstActivity extends AppCompatActivity {
+
+    ActivityResultLauncher<Intent> someActivityResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+                        Intent data = result.getData();
+                        String returnedData = data.getStringExtra("data_return");
+                        Log.d("FirstActivity", returnedData);
+                    }
+                }
+            });
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,6 +36,8 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                someActivityResultLauncher.launch(intent);
             }
         });
     }
