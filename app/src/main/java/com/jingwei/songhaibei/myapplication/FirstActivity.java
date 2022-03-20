4da9636db6aca5f66a851a7e9dee7aa89015528e
songhaibei <songhaibei7305@163.com>
2022-03-20 20:12:35 +0800

    添加Toast

--- app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
@@ -5,6 +5,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class FirstActivity extends AppCompatActivity {
 
@@ -16,6 +17,7 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
             }
         });
     }
