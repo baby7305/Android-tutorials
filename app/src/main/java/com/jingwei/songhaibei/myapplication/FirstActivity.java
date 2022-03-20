ddc43e84e0aec4cff0627e7032c4070973c12d3d
songhaibei <songhaibei7305@163.com>
2022-03-20 16:36:00 +0800

    添加activity Java文件

---
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
@@ -0,0 +1,13 @@
+package com.jingwei.songhaibei.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FirstActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
