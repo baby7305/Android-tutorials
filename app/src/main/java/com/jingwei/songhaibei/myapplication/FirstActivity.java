75fa475c6433a83a13251333f68679b1456ec4c9
songhaibei <songhaibei7305@163.com>
2022-03-20 16:51:40 +0800

    Java加载布局

--- app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
@@ -9,5 +9,6 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.first_layout);
     }
 }
