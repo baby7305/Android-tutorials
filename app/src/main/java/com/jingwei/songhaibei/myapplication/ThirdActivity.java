d1c2017d10a74514fa1849a8c9b74d606520aef1
songhaibei <songhaibei7305@163.com>
2022-03-20 20:07:36 +0800

    新建ThirdActivity

---
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/ThirdActivity.java
@@ -0,0 +1,14 @@
+package com.jingwei.songhaibei.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ThirdActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.third_layout);
+    }
+}
