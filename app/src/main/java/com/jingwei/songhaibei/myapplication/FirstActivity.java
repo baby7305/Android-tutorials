784eed19333eb17f375a53f4789570a3ea37989b
songhaibei <songhaibei7305@163.com>
2022-03-20 21:13:16 +0800

    拨打电话

--- app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
@@ -2,6 +2,8 @@ package com.jingwei.songhaibei.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +18,9 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(Intent.ACTION_DIAL);
+                intent.setData(Uri.parse("tel:10086"));
+                startActivity(intent);
             }
         });
     }
