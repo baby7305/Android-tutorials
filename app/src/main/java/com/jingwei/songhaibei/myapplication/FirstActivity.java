c050b120ea2c9d6f01c06eea2d513914df71dafd
songhaibei <songhaibei7305@163.com>
2022-03-20 23:27:45 +0800

    返回数据给上一个活动

--- app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
@@ -1,8 +1,11 @@
 package com.jingwei.songhaibei.myapplication;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -16,7 +19,23 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                startActivityForResult(intent, 1);
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case 1:
+                if (resultCode == RESULT_OK) {
+                    String returnedData = data.getStringExtra("data_return");
+                    Log.d("FirstActivity", returnedData);
+                }
+                break;
+            default:
+        }
     }
 }
