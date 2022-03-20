be4d7587162b692deb550ee84f0bcf3fd344e933
songhaibei <songhaibei7305@163.com>
2022-03-20 21:31:16 +0800

    向下一个活动传递数据

--- app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
+++ app/src/main/java/com/jingwei/songhaibei/myapplication/FirstActivity.java
@@ -2,6 +2,7 @@ package com.jingwei.songhaibei.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -16,6 +17,10 @@ public class FirstActivity extends AppCompatActivity {
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                String data = "Hello SecondActivity";
+                Intent intent = new Intent(FirstActivity.this, SecondActivity.class);
+                intent.putExtra("extra_data", data);
+                startActivity(intent);
             }
         });
     }
